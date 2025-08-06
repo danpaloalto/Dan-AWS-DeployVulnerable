bef8f053c54558f635e5cdcc2e35afc43b37def5
danpaloalto <155748033+danpaloalto@users.noreply.github.com>
2025-08-06 11:24:48 -0400

    Create identity.tf

---
+++ terraform/identity.tf
@@ -0,0 +1,51 @@
+# insecure-iam.tf
+
+resource "aws_iam_user" "insecure_user" {
+  name = "insecure-user"
+  # No MFA enforced
+}
+
+resource "aws_iam_access_key" "insecure_user_key" {
+  user = aws_iam_user.insecure_user.name
+}
+
+resource "aws_iam_policy" "insecure_policy" {
+  name        = "insecure-policy"
+  description = "Overly permissive policy to trigger CNAPP findings"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "*"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "insecure_role" {
+  name               = "insecure-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "insecure_role_attach" {
+  role       = aws_iam_role.insecure_role.name
+  policy_arn = aws_iam_policy.insecure_policy.arn
+}
+
+resource "aws_iam_instance_profile" "insecure_instance_profile" {
+  name = "insecure-instance-profile"
+  role = aws_iam_role.insecure_role.name
+}
