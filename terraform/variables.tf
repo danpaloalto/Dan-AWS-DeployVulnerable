bdc179874edb0ebfedafd8a20b98ef3e0a33f9d5
danpaloalto <155748033+danpaloalto@users.noreply.github.com>
2025-08-07 05:35:32 -0400

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_id" {
+  description = "The ID of the VPC where resources will be deployed."
+  type        = string
+}
+
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance."
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The ID of the public subnet where resources will be deployed."
+  type        = string
+}
+
+variable "iam_instance_profile_name" {
+  description = "The name of the IAM instance profile for the EC2 instance."
+  type        = string
+}
